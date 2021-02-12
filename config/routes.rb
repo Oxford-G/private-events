1c31614b714c8bdddf033a37a78745aeb1a429e9
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-02-12 11:53:02 +0100

    add event controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, except:[:destroy, :edit]
   resources :sessions, only:[:new, :create, :destroy]
+  resources :events
   root to: 'users#new'
 end
