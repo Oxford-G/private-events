d3cd8f6f6ef1f5a3d7b6521b12b5cc76e8585d2a
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-02-13 10:24:26 +0100

    add validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,10 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name:'User'
   has_many :invitation
   has_many :attendee, through: :invitation, source: :user
+
+  scope :upcoming_events, -> { where('date >= ?', Time.now) }
+  scope :previous_events, -> { where('date <  ?', Time.now) }
+
+  validates :date, presence: true
+  validates :name, presence: true
 end
