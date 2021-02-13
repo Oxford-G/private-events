7c211af9a3c68f608a9399859f42beb9e28658ae
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-02-13 10:25:14 +0100

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :events, foreign_key: :creator_id
   has_many :invitation
   has_many :attended_events, through: :invitation, source: :user
+  validates :name, presence: true, uniqueness: true
 end
