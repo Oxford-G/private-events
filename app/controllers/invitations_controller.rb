0469fccb899fbc603840822e7dc2291634cb1199
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-02-15 19:38:30 +0100

    updated invitations controller

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,15 +1,44 @@
 class InvitationsController < ApplicationController
-  include InvitationsHelper
-  def new
-    @invite = Invitation.new
-  end
 
   def create
-    @invite = Invitation.create(invitation_params)
-    if @invite.save
-      redirect_to event_path(@invite.event_id), notice: 'Invitation sent'
+    event = Event.find(params[:event_id])
+    invitation = Invitation.new(event_id: event.id, user_id: params[:user_id])
+
+    if invitation.save
+      invitation.invited!
+      flash[:notice] = "Invitation sent!"
+      redirect_to users_path(event_id: event.id)
     else
-      redirect_to event_path(@invite.event_id), alert: 'Error creating invitation!'
+      flash[:alert] = 'Ooops! Something went wrong...'
+      redirect_to event_path(event)
     end
+  end
+
+  def destroy
+    event = Event.find(params[:event_id])
+    invitation = Invitation.find(params[:id])
+    if current_user == event.creator
+      invitation.destroy
+      flash[:notice] = "The invitation is cancelled!"
+    else
+      invitation.invited!
+      # enrollment.save
+      flash[:notice] = "You have dropped the enrollment for the #{event.name}!"
+    end
+
+    redirect_to users_path(event_id: event.id, id: enrollment.id)
+  end
+
+  def update
+    @event = Event.find(params[:event_id])
+    @invitation = Invitation.find_by(event_id: params[:event_id], user_id: current_user.id)
+    if @invitation && @invitation.invited?
+      @invitation.accepted!
+      flash[:notice] = "Thank you for signing up for the '#{@event.name}'!"
+    else
+      flash[:alert] = 'Your name is not on the invitation list'
+    end
+
+    redirect_to event_path(@event)
   end
 end
