89ffbbc317f2762da4b9be778bb8972ca5710961
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-02-15 19:40:36 +0100

    add foreign key to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-  # has_one :session
   has_many :events, foreign_key: :creator_id
   has_many :invitations
-  has_many :attended_events, through: :invitations, source: :user
+  has_many :attended_events, through: :invitations, source: :event
   validates :name, presence: true, uniqueness: true
 end
