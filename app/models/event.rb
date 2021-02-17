c6bb4bc3e7c6fe7ef6a09d1c7ba26a7027be1d03
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-02-17 15:33:15 +0100

    add zone to the time scope

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,8 +3,8 @@ class Event < ApplicationRecord
   has_many :invitations
   has_many :attendee, through: :invitations, source: :user
 
-  scope :upcoming_events, -> { where('date >= ?', Time.now) }
-  scope :previous_events, -> { where('date <  ?', Time.now) }
+  scope :upcoming_events, -> { where('date >= ?', Time.zone.now) }
+  scope :previous_events, -> { where('date <  ?', Time.zone.now) }
 
   validates :date, presence: true
   validates :name, presence: true
